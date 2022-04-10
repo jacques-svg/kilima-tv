03fb671278f88dc5a43a31ea7cfab0617f1dd7df
jacques-svg <soromoudorojacques38@gmail.com>
2022-04-09 22:36:16 -0300

    create views components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvShowsComponent } from './tv-shows/tv-shows.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { MusicsComponent } from './musics/musics.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tv-show', component: TvShowsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'subscriptions', component: SubscriptionsComponent },
+  { path: 'musics', component: MusicsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { TvShowsComponent } from './tv-shows/tv-shows.component';
+import { MusicsComponent } from './musics/musics.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './home/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MoviesComponent,
+    TvShowsComponent,
+    MusicsComponent,
+    SubscriptionsComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
